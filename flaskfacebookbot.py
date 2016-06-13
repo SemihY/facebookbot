60ae3d718b787d29090cf896a903fc8f00dbbce1
SemihY <semihyildiz@yandex.com>
2016-06-13 07:33:11 +0300

    Creating...

--- flaskfacebookbot.py
+++ flaskfacebookbot.py
@@ -5,9 +5,9 @@ from wit import Wit
 app = Flask(__name__)
 
 
-ACCESS_TOKEN = "EAACg9CP50TABAD6Sm8Jz4NVSQObLkGtXL4fPFSIQAxSDzgC5Or8tc2qb8os6pmAVFO7zdCnQZAZB8sLZAsznZCcJ1y9flk5j9rzlnpNtwdFExb3lcF74ghmcZA9sGUfwF8SPqsdZBUnbcYZCSVYlmY1CPUg2GZAlmJUlE5n67z8izwZDZD"
-VERIFY_TOKEN = "my_voice_is_my_password_verify_me"
-WITAI_TOKEN = "O5Q4OV75PDXMSL3FX3FEXRECEBB3SD36"
+ACCESS_TOKEN = ""
+VERIFY_TOKEN = ""
+WITAI_TOKEN = ""
 
 WitAi_returnMessage = ""
 
@@ -25,20 +25,22 @@ def handle_verification():
 def handle_messages():
     print("Handling Messages")
     data = request.json
-    sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
-    print("Incoming from %s: %s" % (sender, message))
-    send_message(sender, message)
-
+    print(data)
+    messaging_events = data["entry"][0]["messaging"][0]
+    print(messaging_events)
+    if "message" in messaging_events and "text" in messaging_events["message"]:
+        send_message(messaging_events["sender"]["id"], messaging_events["message"]["text"].encode('unicode_escape'))
+    else:
+        print("Do nothing")
     return "ok"
 
-
-def send_message(token, recipient, text):
+def send_message(recipient, text):
 
     client = Wit(WITAI_TOKEN, actions)
 
     session_id = 'my-user-id-42'
-    client.run_actions(session_id, text, {})
+    context0 = client.run_actions(session_id, text, {})
+    print(context0)
 
     data = {
         "recipient": {"id": recipient},
@@ -48,12 +50,28 @@ def send_message(token, recipient, text):
     print(resp.content)
 
 
+def first_entity_value(entities, entity):
+    if entity not in entities:
+        return None
+    val = entities[entity][0]['value']
+    if not val:
+        return None
+    return val['value'] if isinstance(val, dict) else val
+
 def say(session_id, context, msg):
     global WitAi_returnMessage
     WitAi_returnMessage = msg
     print(msg)
 
 def merge(session_id, context, entities, msg):
+    math = first_entity_value(entities, 'math_expression')
+    if math:
+        print(math)
+        context['math'] = eval(math)
+
+    hi = first_entity_value(entities, 'usage')
+    if hi:
+        print(hi)
     return context
 
 def error(session_id, context, e):
